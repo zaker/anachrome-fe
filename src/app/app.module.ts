2ba9d942f7cbf97645ca5a78a9140a4d61620e2b
Sondre Naustdal <sondre.naustdal@gmail.com>
2020-08-19 14:09:03 +0200

    Remove unused files and behaviour

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { InMemoryCache } from '@apollo/client/core';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-
-export const environment = {
-  production: true,
-  api_address: document.head.querySelector("[property~=apiUrl][content]")
-                   .getAttribute("content")
-};
